b9e683bbc00bd427234c70eb70d5b4914e197a8d
qapquiz <arm.armariya@gmail.com>
2024-08-07 00:48:06 +0700

    feat: add barebone vim config with lazy.nvim

---
+++ lua/vim_options.lua
@@ -0,0 +1,16 @@
+vim.cmd('set noexpandtab')
+vim.cmd('set tabstop=2')
+vim.cmd('set softtabstop=2')
+vim.cmd('set shiftwidth=2')
+
+vim.cmd('set relativenumber')
+
+vim.g.mapleader = ' '
+
+-- mappings
+local map = vim.keymap.set
+
+map('n', '<TAB>', '<cmd>bnext<cr>', { desc = 'Go to next buffer' })
+map('n', '<S-TAB>', '<cmd>bprevious<cr>', { desc = 'Go to previes buffer' })
+map('n', '<leader>x', '<cmd>bdelete<cr>', { desc = 'Delete Buffer' })
+
