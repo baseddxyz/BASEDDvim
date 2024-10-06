694da2c62bb8fcc9bdfa53a26cfde787dac34425
qapquiz <arm.armariya@gmail.com>
2024-10-07 00:02:55 +0700

    fix: remove nvim-tree/nvim-web-devicons to use mini.icons instead

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -59,17 +59,11 @@ return {
 		'echasnovski/mini.statusline',
 		version = false,
 		config = true,
-		dependencies = {
-			{ "nvim-tree/nvim-web-devicons" },
-		},
 	},
 	{
 		'echasnovski/mini.tabline',
 		version = false,
 		config = true,
-		dependencies = {
-			{ "nvim-tree/nvim-web-devicons" },
-		},
 	},
 	{
 		'echasnovski/mini.pairs',
