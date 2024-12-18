f4e5c28aa82bef4b8fca413ae2dcaa2c74c9fc3c
qapquiz <arm.armariya@gmail.com>
2024-12-19 00:19:27 +0700

    fix: change layout of fzf to default as telescope and disable which-key

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -3,9 +3,9 @@ return {
 		"ibhagwan/fzf-lua",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		keys = {
-			{ "<leader>ff", "<cmd>lua require('fzf-lua').files({ fzf_opts = { ['--layout'] = 'reverse-list' } })<cr>" },
-			{ "<leader>fw", "<cmd>lua require('fzf-lua').live_grep_native({ fzf_opts = { ['--layout'] = 'reverse-list' } })<cr>" },
-			{ "<leader>fb", "<cmd>lua require('fzf-lua').buffers({ fzf_opts = { ['--layout'] = 'reverse-list' } })<cr>" },
+			{ "<leader>ff", "<cmd>lua require('fzf-lua').files({ fzf_opts = { ['--layout'] = 'default' } })<cr>" },
+			{ "<leader>fw", "<cmd>lua require('fzf-lua').live_grep_native({ fzf_opts = { ['--layout'] = 'default' } })<cr>" },
+			{ "<leader>fb", "<cmd>lua require('fzf-lua').buffers({ fzf_opts = { ['--layout'] = 'default' } })<cr>" },
 		},
 		config = true,
 	}
