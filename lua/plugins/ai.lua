e07a53a9ffc44d9a22d9e384e54084be46922240
qapquiz <arm.armariya@gmail.com>
2024-12-29 15:51:56 +0700

    feat(CodeCompanion): use cmd to lazy load code companion

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -16,6 +16,12 @@ return {
 	},
 	{
 		"olimorris/codecompanion.nvim",
+		cmd = {
+			'CodeCompanion',
+			'CodeCompanionActions',
+			'CodeCompanionChat',
+			'CodeCompanionCmd',
+		},
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"nvim-treesitter/nvim-treesitter",
