714ed365fd6c3b83606839371fabdc2371ce0658
qapquiz <arm.armariya@gmail.com>
2025-05-05 22:59:10 +0700

    feat: add obsidian.nvim

---
+++ lua/plugins/note-taking.lua
@@ -0,0 +1,26 @@
+return {
+	"obsidian-nvim/obsidian.nvim",
+	version = "*",
+	lazy = true,
+	ft = "markdown",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"saghen/blink.cmp", -- optional
+		"folke/snacks.nvim", -- optional
+	},
+	opts = {
+		workspaces = {
+			{
+				name = "personal",
+				path = "~/vaults/personal",
+			},
+		},
+		completion = {
+			nvim_cmp = false,
+			blink = true,
+		},
+		picker = {
+			name = "snacks.pick",
+		}
+	},
+}
