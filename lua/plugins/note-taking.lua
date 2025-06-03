a21c45f4eb6c5c24698ba4b42c784bcddbe4793b
qapquiz <arm.armariya@gmail.com>
2025-06-04 02:26:20 +0700

    feat: add checkmate.nvim for better todo

--- lua/plugins/note-taking.lua
+++ lua/plugins/note-taking.lua
@@ -1,26 +1,33 @@
 return {
-	"obsidian-nvim/obsidian.nvim",
-	version = "*",
-	lazy = true,
-	ft = "markdown",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"saghen/blink.cmp", -- optional
-		"folke/snacks.nvim", -- optional
-	},
-	opts = {
-		workspaces = {
-			{
-				name = "personal",
-				path = "~/vaults/personal",
+	{
+		"obsidian-nvim/obsidian.nvim",
+		version = "*",
+		lazy = true,
+		ft = "markdown",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"saghen/blink.cmp", -- optional
+			"folke/snacks.nvim", -- optional
+		},
+		opts = {
+			workspaces = {
+				{
+					name = "personal",
+					path = "~/vaults/personal",
+				},
 			},
+			completion = {
+				nvim_cmp = false,
+				blink = true,
+			},
+			picker = {
+				name = "snacks.pick",
+			}
 		},
-		completion = {
-			nvim_cmp = false,
-			blink = true,
-		},
-		picker = {
-			name = "snacks.pick",
-		}
 	},
+	{
+		"bngarren/checkmate.nvim",
+		ft = "markdown",
+		opts = {},
+	}
 }
