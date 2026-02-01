0cbe2a919ee7ff2fa08e95ef68853d9bbaedf3eb
qapquiz <arm.armariya@gmail.com>
2026-02-01 14:02:33 +0700

    feat: add 99 and remove note-taking

--- lua/plugins/note-taking.lua
+++ lua/plugins/note-taking.lua
@@ -1,34 +1,34 @@
 return {
-	{
-		"obsidian-nvim/obsidian.nvim",
-		version = "*",
-		lazy = true,
-		ft = "markdown",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"saghen/blink.cmp", -- optional
-			"folke/snacks.nvim", -- optional
-		},
-		opts = {
-			workspaces = {
-				{
-					name = "personal",
-					path = "~/vaults/personal",
-				},
-			},
-			completion = {
-				nvim_cmp = false,
-				blink = true,
-			},
-			picker = {
-				name = "snacks.pick",
-			},
-			legacy_commands = false,
-		},
-	},
-	{
-		"bngarren/checkmate.nvim",
-		ft = "markdown",
-		opts = {},
-	}
+	-- {
+	-- 	"obsidian-nvim/obsidian.nvim",
+	-- 	version = "*",
+	-- 	lazy = true,
+	-- 	ft = "markdown",
+	-- 	dependencies = {
+	-- 		"nvim-lua/plenary.nvim",
+	-- 		"saghen/blink.cmp", -- optional
+	-- 		"folke/snacks.nvim", -- optional
+	-- 	},
+	-- 	opts = {
+	-- 		workspaces = {
+	-- 			{
+	-- 				name = "personal",
+	-- 				path = "~/vaults/personal",
+	-- 			},
+	-- 		},
+	-- 		completion = {
+	-- 			nvim_cmp = false,
+	-- 			blink = true,
+	-- 		},
+	-- 		picker = {
+	-- 			name = "snacks.pick",
+	-- 		},
+	-- 		legacy_commands = false,
+	-- 	},
+	-- },
+	-- {
+	-- 	"bngarren/checkmate.nvim",
+	-- 	ft = "markdown",
+	-- 	opts = {},
+	-- }
 }
